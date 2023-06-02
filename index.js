a1762bfc9ef983ba3b9c17f6fc5c0638f09211ac
kaboyokelvin <kaboyokelvin@gmail.com>
2023-06-03 02:46:49 +0300

    add dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('@babel/register');
+require('dotenv').config()
 
 const app =require('./src/app');
 
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://kaboyokelvin:0774466063@cluster0.wtfh4c0.mongodb.net/').catch(error=>console.log(error.message))
+mongoose.connect(process.env.MONGO_URI).catch(error=>console.log(error.message))
 
 app.post('/signup', async(req, res) => {
     const newuser = await user.create({email:req.body.email,password:req.body.password});
