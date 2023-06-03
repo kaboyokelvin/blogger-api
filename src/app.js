905910bcad4b2b9d354c5c011c5e866631039e10
kaboyokelvin <kaboyokelvin@gmail.com>
2023-06-04 02:09:43 +0300

    added login route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const user = require('./models/users');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -25,8 +26,8 @@ app.post('/signup', async (req, res, next) => {
                 const hashedPassword = await bcrypt.hash(password, 10);
                 await user.create({ email: userEmail, password: hashedPassword });
                 res.status(201).json({ message: "User created" });
-            } catch (error) {
-                next(error)
+            } catch (err) {
+                next(err)
             }
         }
     }
@@ -36,6 +37,33 @@ app.post('/signup', async (req, res, next) => {
 
 })
 
+app.post('/signin', async (req, res, next) => {
+    const userEmail = req.body.email;
+    const password = req.body.password;
+    if (validator.isEmail(userEmail)) {
+        const checkEmail = await user.findOne({ email: userEmail })
+        if (checkEmail) {
+            const match = await bcrypt.compare(password, checkEmail.password);
+            if (match) {
+                const token = jwt.sign({userEmail,password},process.env.SECRET);
+                res.json(token);
+                res.json("successfully logged in");
+            }
+            else {
+                res.json({ message: "incorrect password entered" });
+            }
+
+        }
+        else {
+            res.json({ message: "Email does not exist" });
+        }
+
+
+    }
+    else {
+        res.json({ message: "invalid email format" });
+    }
+})
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).json({ message: 'Something broke!', error: err.message })
