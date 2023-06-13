e5955656c693d75212dd696585ff96c90fb7ae60
kaboyokelvin <99651351+kaboyokelvin@users.noreply.github.com>
2023-06-13 13:43:20 +0300

    modularize app (#10)
    * modularize app
    
    * add users.auth prefix
    
    * modularize
    
    ---------
    
    Co-authored-by: nyabongoedgar <nyabongoedgar@gmail.com>

---
+++ src/middleware/auth/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+export default function verifyToken (req, res, next) {
+  const authHeader = req.headers.authorization
+  if (authHeader !== 'undefined') {
+    const token = authHeader.split(' ')[1]
+
+    try {
+      const decodedJWT = jwt.verify(token, process.env.SECRET)
+      req.userId = decodedJWT._id
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+  return res.status(401).json({
+    message: 'jwt required'
+  })
+}
